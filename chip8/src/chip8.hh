c6e1f0e59d5101ed79482bc32d254a6b1ed27d8e
Arttu Multanen <karethoth@gmail.com>
2014-10-20 23:20:49 +0300

    Updated CPU structure.

--- chip8/src/chip8.hh
+++ chip8/src/chip8.hh
@@ -2,9 +2,16 @@
 
 namespace Chip8Emu
 {
+	// Typedefs
 	typedef unsigned char   u8;
 	typedef unsigned short u16;
 
+
+	// Constants
+	const u16 programStart = 0x200;
+
+
+	// Helper Functions
 	template <typename T>
 	bool GetBit( T src, u8 offset )
 	{
@@ -12,10 +19,23 @@ namespace Chip8Emu
 	}
 
 
+	// The CPU Structure
 	struct CPU
 	{
-		// General purpose registers
+		// General Purpose Registers
 		u8 V0, V1, V2, V3, V4, V5, V6, V7,
 		   V8, V9, VA, VB, VC, VD, VE, VF;
+
+		// Special registers
+		u16 I;               // Address Register
+		u8  Time, Tone;      // Time and Tone Registers
+		u16 PC;              // Program Counter
+		u8  SP;              // Stack Pointer
+		u16 Key;             // Key Register (for input)
+
+		// Memory
+		u16 stack[16];       // Stack
+		u8  ram[0x1000];     // System RAM
+
 	};
 };
